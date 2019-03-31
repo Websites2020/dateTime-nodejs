32452b50a3f2a5f6c0e63906d3a34f96e0617eab
SOIP <38543876+sumofitsparts@users.noreply.github.com>
2019-03-31 15:49:59 -0400

    first

---
+++ demo_module.js
@@ -0,0 +1,9 @@
+var http = require ('http');
+var dateTime = require ('./myFirstModule.js');
+
+http.createServer(function(req, res) {
+    res.writeHead(200, {'Content-Type':'text/html'});
+    res.write("The date and time are currently: " + dateTime.myDateTime());
+    res.end();
+}).listen(8080);
+
